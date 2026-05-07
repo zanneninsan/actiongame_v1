de1637220765620dec0682d4a236605997cc8bce
zanneninsan <zanneninsan@users.noreply.github.com>
2026-05-07 04:00:01 +0000

    deploy: ba2b4f2bdcaf5ce249455ea5c682b9c3ab4172a5

---
+++ pr-3/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "zannenin-land-shell-v2";
+const SHELL_URLS = [
+  "./",
+  "./manifest.webmanifest",
+  "./favicon.ico",
+  "./assets/pwa-icon-32.png",
+  "./assets/pwa-icon-180.png",
+  "./assets/pwa-icon-192.png",
+  "./assets/pwa-icon-512.png",
+  "./assets/pwa-icon-maskable-192.png",
+  "./assets/pwa-icon-maskable-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_URLS)).catch(() => undefined));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => undefined);
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./"))),
+  );
+});
